e2d2b1e8a7c8c3db78ff92e8b358878650ef22f3
Kill00 <kill00@p.teamvery.dev>
2021-12-07 19:14:05 +0900

    reference

---
+++ Assets/Lab/Scripts/PlayerMovement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+namespace Lab.Scripts {
+public class PlayerMovement : MonoBehaviour {
+    private void Start() {
+        throw new NotImplementedException();
+    }
+
+    private void Update() {
+        throw new NotImplementedException();
+    }
+}
+}
