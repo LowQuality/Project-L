ce1de60d9bc802ad0c44bddcb28718e00aed62cd
Kill00 <kill00@p.teamvery.dev>
2021-12-07 20:54:29 +0900

    Player Interaction Support (Movement, CameraMovement)

---
+++ Assets/Lab/Scripts/Player/CameraMovement.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+
+namespace Lab.Scripts.Player {
+public class CameraMovement : MonoBehaviour {
+    // Sensitivity of mouse movement
+    [SerializeField] private float lookSensitivity;
+
+    // Camera Rotation Limit
+    [SerializeField] private float cameraRotationLimit;
+    private float _currentCameraRotationX;
+    
+    // Component
+    [SerializeField] private Camera getCamera;
+    
+    private Rigidbody _rigidbody;
+    
+    private void Start() {
+        getCamera = FindObjectOfType<Camera>();
+        _rigidbody = GetComponent<Rigidbody>();
+        
+        // Hide and lock cursor
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    private void Update() {
+        // Camera Movement
+        CameraRotation();
+        CharacterRotation();
+    }
+    
+    private void CameraRotation() {
+        var xRotation = Input.GetAxisRaw("Mouse Y");
+        var cameraRotationX = xRotation * lookSensitivity;
+
+        _currentCameraRotationX -= cameraRotationX;
+        _currentCameraRotationX = Mathf.Clamp(_currentCameraRotationX, -cameraRotationLimit, cameraRotationLimit);
+
+        getCamera.transform.localEulerAngles = new Vector3(_currentCameraRotationX, 0f, 0f);
+    }
+
+    private void CharacterRotation() {
+        var yRotation = Input.GetAxisRaw("Mouse X");
+        var characterRotationY = new Vector3(0f, yRotation, 0f) * lookSensitivity;
+
+        _rigidbody.MoveRotation(_rigidbody.rotation * Quaternion.Euler(characterRotationY));
+    }
+}
+}
---
+++ Assets/Lab/Scripts/Player/Movement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Lab.Scripts.Player {
+public class Movement : MonoBehaviour {
+    [SerializeField] private float walkSpeed;
+    [SerializeField] private float runSpeed;
+    private float _applySpeed;
+
+    private bool _isRun;
+
+    private void Start() {
+        _applySpeed = walkSpeed;
+    }
+
+    private void Update() {
+        // Player movement
+        Move();
+        Run();
+    }
+    
+    private void Run() {
+        if (Input.GetKey(KeyCode.LeftShift)) {
+            Running();
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift)) {
+            RunningCancel();
+        }
+    }
+    
+    private void Move() {
+        var moveDirX = Input.GetAxis("Horizontal");
+        var moveDirZ = Input.GetAxis("Vertical");
+        
+        transform.Translate(moveDirX * _applySpeed * Time.deltaTime, 0, moveDirZ * _applySpeed * Time.deltaTime);
+    }
+    
+    private void Running() {
+        _isRun = true;
+        _applySpeed = runSpeed;
+    }
+
+    private void RunningCancel() {
+        _isRun = false;
+        _applySpeed = walkSpeed;
+    }
+}
+}
